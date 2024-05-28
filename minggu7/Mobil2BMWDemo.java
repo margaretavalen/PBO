9d758fb079cbc840d10eb746526399f8c7e89d8d
Margareta Valencia <113288412+margaretavalen@users.noreply.github.com>
2024-05-28 20:23:52 +0700

    Delete minggu7 directory

--- minggu7/Mobil2BMWDemo.java
+++
@@ -1,9 +0,0 @@
-public class Mobil2BMWDemo {
-	public static void main(String[] a){
-		Mobil2BMW mobilku = new Mobil2BMW();
-		mobilku.hidupkanMobil ("Silver"); //parent
-		mobilku.nontonTV(); //child
-		mobilku.ubahGigi("Silver"); //parent
-		mobilku.matikanMobil("Silver"); //parent
-	}
-}
--- minggu7/MountainBike.java
+++
@@ -1,7 +0,0 @@
-class MountainBike extends Bicycle{
-	int seatHeight;
-	public void setHeight(int newValue) {
-		seatHeight = newValue;
-		System.out.println("Seat Height:"+seatHeight);
-	}
-}
